9d54fd7a910ab6cb0afb049bc903b02d71e52b9e
Andrew C <acchang4@gmail.com>
2020-04-18 19:07:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,18 +11,3 @@ class Dog
     @name
   end
 end
-
-
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-end
